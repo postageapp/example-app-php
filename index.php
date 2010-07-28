2d368f7543794e87edced7dc09144429ca0a3982
Oleg <oleg@theworkinggroup.ca>
2010-07-28 12:16:40 -0400

    made it a bit nicer

--- index.php
+++ index.php
@@ -1,94 +1,8 @@
 <?php 
   require_once('postageapp_class.inc');
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xml:lang='en' xmlns='http://www.w3.org/1999/xhtml'>
-  <head>
-    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-    <link rel="stylesheet" href="stylesheets/main.css" type="text/css" media="screen" charset="utf-8" />
-    <title>Php example for PostageApp</title>
-  </head>
-<body>
-  <div class="content_wrap">
-    <div class="header">
-      <img src="images/postageapp_php.gif" alt="Postageapp_php"/>
-      <h1>Php example for PostageApp<br/><a target="_blank" class="small" href="">http://postageapp.com/docs</a></h1>
-    </div>
   
-    <div class="content">
-      <?php
-        if (isset($_POST['email']) && $_POST['email'] !='') {
-          # Processes the form if the email has been entered (see below)
-          $response = process_submited_form();
-        }
-      ?>
-      <h2>Fill in the form on the right to send a message through PostageApp</h2>
-
-      <div class="left_column">
-        <?php
-          if (POSTAGE_API_KEY == 'ENTER YOUR API KEY HERE') {
-            echo '
-              <p class="warning">
-                <img src="images/bullet_error.png" />
-                Before sending a message you\'ll need to add your API key to  postageapp_conf.inc and restart your server.
-              </p>
-            ';
-          } else {
-            echo '<p>Your API key is: <span class="api_key">'.POSTAGE_API_KEY.'</span></p>';
-          }
-        
-        ?>
-        <p>
-          Enter a subject, your email address and a value for the <span class="variable">{{name}}</span> variable.
-        </p>
-        
-        <?php
-          if($response!=null) {
-            echo '
-              <div class="response">
-                <p>Here is the response received from PostageApp:</p>
-            ';
-            echo '<pre class="debug_dump">';
-            print_r ($response);
-            echo '</pre></div>';
-          }
-        
-        ?>
-      </div>
-
-      <div class="right_column">
-        <form method="post">
-          <p><label>Subject:</label> <input type="text" value="My name is {{name}}" name="subject"/></p>
-          <p><label>My email address:</label> <input type="text" name="email"/></p>
-          <p><label><span class="variable">{{name}}</span> variable:</label> <input type="text" name="variable"/></p>
-          <p>
-            <label>Plain text content:</label>
-            <br/>
-            <textarea rows="5" name="plain_text_content" cols="40">Hi,
-  My name is {{name}} and this is a test.</textarea>
-          </p>
-          <p>
-            <label>HTML content:</label> 
-            <br/>
-            <textarea rows="5" name="html_text_content" cols="40">&lt;p&gt;Hi,&lt;/p&gt;
-  &lt;p&gt;My name is &lt;b&gt;{{name}}&lt;/b&gt; and this is a test.&lt;/p&gt;
-            </textarea>
-          </p>
-          <p><label> </label><input type="submit" value="Send message" name="commit"/></p>
-        </form>
-      </div>
-    </div>
-    <div class="footer">
-      developed by THE WORKING GROUP
-    </div>
-  </div>
-</body>
-</html>
-
-<?php
   function process_submited_form() {
-
+    
     # Who's going to receive this email.
     # The $to field can have the following formats:
     #
@@ -114,23 +28,83 @@
       'From'      => 'my_test@somewhere.com',
       'Reply-to'  => 'my_test@somewhere.com'
     );
-
+    
     # The body of the message
     $mail_body = array(
       'text/plain' => $_POST['plain_text_content'],
       'text/html' => $_POST['html_text_content']
     );
-  
+    
     # Send it all
     $response = PostageApp::mail($to, $subject, $mail_body, $header);
-    
-    # Analyze the response
-    if ($response->response->status == 'ok') {
-      echo '<div class="flash_message notice">Your message has been sent. Check your project in PostageApp</div>';
-    } else {
-      echo '<div class="flash_message error">There was an error sending your message</div>';
-    }
     return $response;
   }
+  
+  if (isset($_POST['email']) && $_POST['email'] !='') {
+    # Processes the form if the email has been entered (see below)
+    $response = process_submited_form();
+  }
+  
+  $api_key = (POSTAGE_API_KEY == 'ENTER YOUR API KEY HERE') ? null : POSTAGE_API_KEY
+  
 ?>
 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xml:lang='en' xmlns='http://www.w3.org/1999/xhtml'>
+  <head>
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" href="stylesheets/main.css" type="text/css" media="screen" charset="utf-8" />
+    <title>PHP Example Application for PostageApp</title>
+  </head>
+  
+  <body>
+    <div class='body_wrapper'>
+      <h1> 
+        PHP Example Application for PostageApp
+      </h1>
+      
+      <div class="api_key">
+        <?php if($api_key){ ?>
+          API KEY: <span><?= $api_key ?></span>
+        <?php } else { ?>
+          Please insert your project API KEY in <span>postageapp_conf.inc<span>
+        <?php } ?>
+      </div>
+      
+      <?php if($response){ ?>
+        <div class='response'>
+          <pre><?= print_r ($response); ?></pre>
+        </div>
+      <?php } ?>
+      
+      <form method="post">
+        <label> Subject
+          <input type="text" value="Hello {{name}}" name="subject"/>
+        </label>
+        <label> To
+          <input type="text" value="me@example.com" name="email"/>
+        </label>
+        <label> Name
+          <input type="text" value="Tester" name="variable"/>
+        </label>
+        <label> HTML Content
+          <textarea rows="5" name="html_text_content" cols="40">&lt;h1&gt;Hi, {{name}}&lt;/h1&gt;
+&lt;p&gt;This is a test.&lt;/p&gt;
+          </textarea>
+        </label>
+        <label> Plain Text Content
+          <textarea rows="5" name="plain_text_content" cols="40">Hi, {{name}}
+This is a test
+          </textarea>
+        </label>
+        <input type="submit" value="Send Message" name="commit"/>
+      </form>
+      
+      <ul>
+        <li><a href='http://postageapp.com'>PostageApp</a></li>
+        <li><a href='http://help.postageapp.com'>PostageApp Help Portal</a></li>
+      </ul>
+    </div>
+  </body>
+</html>
